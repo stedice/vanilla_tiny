396f73912985f46af4d5447763134786440c3370
stedice <stefanodc@mela2.fritz.box>
2017-12-05 15:51:10 +0100

    left menu styling

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,6 +1,6 @@
 <?php if (!$this) { exit(header('HTTP/1.0 403 Forbidden')); } ?>
 
-<div class="container">
+<div id="content">
     <?php         
     	require APP . 'views/home/menu-left.php';
     	require APP . 'views/home/list.php';
--- application/views/home/menu-left.php
+++ application/views/home/menu-left.php
@@ -1,5 +1,10 @@
-<?php
-
-echo 'menu';
-
-?>
+<div id="menu-left">
+      <ul class="menu">
+      	<li>
+      		<button class="btn people active">all contacts</button>
+      	</li>
+      	<li>
+          <button class="btn star">favorites</button>
+        </li>
+      </ul>
+</div>
